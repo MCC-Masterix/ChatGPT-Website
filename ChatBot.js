f99742eb294c839bf0c511bce32bfb31ed417779
Masterix <108416927+MCC-Masterix@users.noreply.github.com>
2023-07-22 19:21:55 +0200

    Update ChatBot.js

--- ChatBot.js
+++ ChatBot.js
@@ -1,50 +1,75 @@
-function appendMessage(message, sender, color) {
-    const chatbox = document.getElementById('chatbox');
-    const messageElement = document.createElement('div');
-    messageElement.className = `message ${sender}`;
-    messageElement.innerText = message;
-    messageElement.style.color = color;
-    chatbox.appendChild(messageElement);
-}
+function appendMessage(message, sender) {
+            const chatbox = document.getElementById('chatbox');
+            const messageElement = document.createElement('div');
+            messageElement.className = `message ${sender}`;
+            messageElement.innerText = message;
+            chatbox.appendChild(messageElement);
+        }
 
-function handleKeyPress(event) {
-    if (event.keyCode === 13) {
-        sendMessage();
-    }
-}
+        async function sendMessage() {
+            const userMessage = document.getElementById('userMessage').value;
+            appendMessage(userMessage, 'user');
 
-async function sendMessage() {
-const userMessage = document.getElementById('userMessage').value;
-appendMessage(userMessage, 'user');
-
-const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+            const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
 const apiKey = document.getElementById('apikeyMessage').value;
 
 try {
-const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-        prompt: 'User: ' + userMessage + '\nAI:',
-        max_tokens: 100
-    })
-});
+                const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${apiKey}`
+                    },
+                    body: JSON.stringify({
+                        prompt: 'User: ' + userMessage + '\nAI:',
+                        max_tokens: 100
+                    })
+                });
+                const data = await response.json();
+                console.log('API Response:', data);
+                if (data.choices && data.choices.length > 0) {
+                    const aiMessage = data.choices[0].text.trim();
+                    appendMessage(aiMessage, 'ai');
+                } else {
+                    appendMessage('Error: Unable to get AI response.', 'ai');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                appendMessage('Error: An error occurred while fetching the AI response.', 'ai');
+            }
 
-const data = await response.json();
-console.log('API Response:', data);
-if (data.choices && data.choices.length > 0) {
-    const aiMessage = data.choices[0].text.trim();
-    appendMessage(aiMessage, 'ai');
-} else {
-    appendMessage('Error: Unable to get AI response.', 'ai', '#FF0000');
-}
-} catch (error) {
-console.error('Error:', error);
-appendMessage('Error: An error occurred while fetching the AI response.', 'ai');
-}
+            document.getElementById('userMessage').value = '';
+        }
+
+        // Function to change H1 color on mouseover
+        function changeColor() {
+            const h1 = document.querySelector('h1');
+            const randomColor = getRandomColor();
+            h1.style.color = randomColor;
+        }
+
+        // Function to reset H1 color on mouseout
+        function resetColor() {
+            const h1 = document.querySelector('h1');
+            h1.style.color = 'rgb(175, 175, 175)';
+        }
+
+        // Function to generate a random color
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        // Handle Enter key press to send message
+        function handleKeyPress(event) {
+            if (event.keyCode === 13) {
+                sendMessage();
+            }
+        }
 
 document.getElementById('userMessage').value = '';
 }
